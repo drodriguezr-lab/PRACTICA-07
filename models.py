2430a6e928409d491251e7fe237ecfd98278520d
drodriguezr-lab <drodriguezr@ciencias.unam.mx>
2026-05-27 20:13:56 -0600

    Parte 2.py

--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 
 
 #Aplica eliminacion gaussiana para convertir la matriz M en una matriz triangular superior. Modifica una copia de la matriz
+#Ejercicio 1
+# Parte 1
 def matrizTriangularSuperior(M):
     # Hacemos una copia para no modificar la matriz original
     A = M.copy().astype(float)  # Convertimos a float para evitar problemas
@@ -15,3 +17,27 @@ def matrizTriangularSuperior(M):
             for j in range(k, n):  # Hacemos cero los elementos debajo del pivote
                 A[i, j] = A[i, j] - factor * A[k, j]
     return A
+
+#Parte 2
+def matrizTriangularSuperior2(T):
+    # Hacemos una copia para no modificar la matriz original
+    A = T.copy().astype(float)  # Convertimos a float para evitar problemas
+    n = A.shape[0]
+    for k in range(n-1,-1,-1):  # Empezamos desde la última fila
+        # Verificamos que el pivote no sea cero (evitamos división por cero)
+        if abs(A[k, k]) < 1e-10:
+            print(f"Advertencia: Pivote casi cero en posición ({k}, {k})")
+            continue 
+        for i in range(k -1, -1,-1):  # Filas por encima de la fila k
+            factor = A[i, k] / A[k, k] 
+            for j in range(k, n):  # Solo necesitamos desde la columna k en adelante
+            
+                A[i, j] = A[i, j] - factor * A[k, j]
+    return A
+
+
+
+
+
+    
+#Parte 3
